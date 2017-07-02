538158dbbb4d1ae389fdceaee0a946b238ed8246
Petra Donka <donkapetra@gmail.com>
2017-07-02 14:40:12 +0200

    ✨ added basic login UI

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Petra Donka on 02.07.17.
+//  Copyright © 2017 Petra Donka. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        if let username = usernameTextField.text, let password = passwordTextField.text {
+            Session.session(forUsername: username, andPassword: password) { result in
+                switch result {
+                case .success(let session):
+                    print(session)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+
+    @IBAction func signupButtonPressed(_ sender: Any) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
