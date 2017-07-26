eb3cefd0c553742c773de67e27e32b034315d82c
Petra Donka <donkapetra@gmail.com>
2017-07-26 19:27:25 +0200

    🔥 removed unnecessary memory warning handler

--- OnTheMap/ViewControllers/ListTableViewController/ListTableViewController.swift
+++ OnTheMap/ViewControllers/ListTableViewController/ListTableViewController.swift
@@ -15,11 +15,6 @@ class ListTableViewController: UITableViewController {
         setupStudentInformations()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
